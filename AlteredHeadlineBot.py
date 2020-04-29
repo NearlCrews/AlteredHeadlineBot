8a1ff42a13cd471c98c3f195959c8a8d37853d91
Nearl Crews <nlabadie@gmail.com>
2020-04-29 15:49:40 -0400

    Initial commit

---
+++ AlteredHeadlineBot.py
@@ -0,0 +1,143 @@
+# Import everything.
+import praw
+import configparser
+import requests
+from bs4 import BeautifulSoup
+import re
+import difflib
+import furl
+import sqlite3
+import os.path
+import time
+
+# Read the configuration.
+config = configparser.ConfigParser()
+config.read('settings.ini')
+reddit_user = config['alteredheadlinebot']['reddit_user']
+reddit_pass = config['alteredheadlinebot']['reddit_pass']
+reddit_client_id = config['alteredheadlinebot']['reddit_client_id']
+reddit_client_secret = config['alteredheadlinebot']['reddit_client_secret']
+reddit_target_subreddit = config['alteredheadlinebot']['reddit_target_subreddit']
+score_threshold = int(config['alteredheadlinebot']['score_threshold'])
+bot_owner = config['alteredheadlinebot']['bot_owner']
+leave_post_comment = config['alteredheadlinebot']['leave_post_comment']
+leave_mod_notice = config['alteredheadlinebot']['leave_mod_notice']
+
+# Regex list of domains to ignore.
+drop_urls = re.compile('(?:.*bloomberg\.com.*|.*reddit\.com.*|.*redd\.it.*|.*imgur\.com.*|.*youtube\.com.*|.*wikipedia\.org.*|.*twitter\.com.*|.*youtu\.be.*|.*facebook\.com.*)', re.IGNORECASE)
+
+# Regex for URL validity.
+valid_url = re.compile('(?:^http.*)', re.IGNORECASE)
+
+# Create the tracking database.
+conn = sqlite3.connect('PostHistory.db')
+c = conn.cursor()
+conn.execute('''CREATE TABLE IF NOT EXISTS ALTEREDHEADLINE
+        (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        EPOCH INTEGER,
+        POSTID TEXT,
+        USERNAME TEXT,
+        POSTTITLE TEXT,
+        POSTURL TEXT,
+        STRIPPEDURL TEXT,
+        URLTITLE TEXT,
+        REDDITURL TEXT);''')
+
+# Create the Reddit object.
+reddit = praw.Reddit(
+    username=reddit_user,
+    password=reddit_pass,
+    client_id=reddit_client_id,
+    client_secret=reddit_client_secret,
+    user_agent='AlteredHeadlineBot by u/{}'.format(bot_owner)
+)
+
+# Start the streaming loop for new submissions.
+while True:
+  for submission in reddit.subreddit(reddit_target_subreddit).stream.submissions():
+    try:
+      # Get the ID.
+      post_id = submission.id
+      if type(post_id) == type(None):
+        continue
+
+      # Check to see if we've already checked this one.
+      c.execute("SELECT id FROM alteredheadline WHERE postid = ?", (post_id,))
+      query_result = c.fetchall()
+      if len(query_result) != 0:
+        continue
+
+      # Username section and error checking.
+      username = submission.author
+      if type(username) == type(None):
+        continue
+
+      # Title section and error checking.
+      post_title = submission.title
+      if type(post_title) == type(None):
+        continue
+
+      # Post URL and error checking.
+      post_url = submission.url
+      if type(post_url) == type(None):
+        continue
+      if not valid_url.match(post_url):
+        continue
+
+      # Create a stripped URL for report checking.
+      stripped_url = furl.furl(post_url).remove(args=True, fragment=True).url
+
+      # Get rid of certain domains that are known to fail.
+      if drop_urls.match(post_url):
+        continue
+
+      # Find the real title from the actual HTML.
+      headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'}
+      real_url_request = requests.get(post_url, headers=headers)
+      html_content = real_url_request.text
+      soup = BeautifulSoup(html_content, 'html.parser')
+      if type(soup.title) == type(None):
+        continue
+      else:
+        url_title = soup.title.string.strip()
+      if len(url_title) <= 16:
+        continue
+
+      # Convert to lowercase and identify similarity.
+      similarity_object = difflib.SequenceMatcher(None, post_title.lower(), url_title.lower())
+      similarity = round(similarity_object.ratio()*100)
+      # DEBUG
+      print(f'Username: {username} \nStripped URL: {stripped_url} \nPost Title: {post_title} \nActual Title: {url_title} \nSimilarity: {similarity} \n' )
+      if (similarity >= score_threshold):
+        continue
+
+      # Send an alert if the title differs significantly.
+      if similarity <= score_threshold and leave_mod_notice:
+        n_link = '[Link to post for review.]({})\n\n'.format(submission.permalink)
+        n_posted = '**Posted Title:** {}\n\n'.format(post_title)
+        n_actual = '**Actual Title:** {}\n\n'.format(url_title)
+        n_similarity = '**Similarity:** {}%\n\n'.format(similarity)
+        n_footer = '\n\n*Please contact u/{} if this bot is misbehaving.*\n\n'.format(bot_owner)
+        notification = n_actual + n_posted + n_similarity + n_link + n_footer
+        reddit.subreddit(reddit_target_subreddit).message('Potentially Altered Headline', notification)
+
+      # Leave a comment in the thread.
+      if similarity <= score_threshold and leave_post_comment:
+        r_message = 'Hello u/{}!\n\n The title of your post differs from the actual article title and has been flagged for review. Please review [Rule #6](https://www.reddit.com/r/Michigan/wiki/index#wiki_rules) in the r/Michigan subreddit rules. If this is an actual rule violation, you can always delete the submission and resubmit with the correct headline. Otherwise, this will likely be removed by the moderators. Please note that some websites change their article titles and this may be a false-positive. In that case, no further action is required. Further details: \n\n'.format(username)
+        n_posted = '**Posted Title:** {}\n\n'.format(post_title)
+        n_actual = '**Actual Title:** {}\n\n'.format(url_title)
+        n_similarity = '**Similarity:** {}%\n\n'.format(similarity)
+        n_footer = '\n\n*Please contact u/{} if this bot is misbehaving.*\n\n'.format(bot_owner)
+        comment_text = r_message + n_posted + n_actual + n_similarity + n_footer
+        post_submission = reddit.submission(id=post_id)
+        this_comment = post_submission.reply(comment_text)
+        this_comment.mod.distinguish(how='yes')
+
+      # Insert into the database.
+      epoch = time.time()
+      c.execute("INSERT INTO alteredheadline (epoch, postid, username, posttitle, posturl, strippedurl, urltitle, redditurl) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (int(epoch), str(post_id), str(username), str(post_title), str(post_url), str(stripped_url), str(url_title), str(submission.permalink)))
+      conn.commit()
+    except:
+      continue
+
+conn.close()
---
+++ README.md
@@ -0,0 +1,103 @@
+# AlteredHeadlineBot
+A PRAW Reddit bot to check for editorialized and altered headlines. 
+
+# Summary
+This simple Reddit PRAW bot is designed for a specific purpose:
+1. Check the "real" headline of a Reddit post against the submitted headline.
+2. Compare the two to check if the difference exceeds a configurable value between 0-100.
+3. Alert the moderators of the subreddit so they can review the submission. 
+4. Leave an optional comment on the post. 
+
+Be gentle. I've never written anything in Python and I'm sure there are more efficient ways of doing everything. This was a "COVID-19 I'm bored" learning project. Either way, it seems to work as expected and we're receiving actionable information.
+
+# Installation
+I did this in Ubuntu 19.10 and used APT to install the required modules:
+```
+sudo apt install python-configparser python3-bs4 python3-furl python3-praw
+```
+
+You want to plug the correct values into `settings.ini`. They're fairly simple:
+```
+[alteredheadlinebot]
+ # Bot username and password, or use your own.
+ reddit_user =
+ reddit_pass =
+ # Create the secret OATH values. Instructions on Reddit.
+ reddit_client_id =
+ reddit_client_secret =
+ # Subreddit to monitor, e.g. Michigan.
+ reddit_target_subreddit =
+ # Score for how "different" the topic and submissions are. High number = more identical. From 0-100.
+ score_threshold = 50
+ # Bot owner. This is typically your username. Don't add the u/ in front of the name.
+ bot_owner =
+ # Set to true if you'd like the bot to leave a comment on the post.
+ # You'll need to edit the message in the Python script.
+ leave_post_comment = True
+ # Set to true if you'd like a mod notice sent to the subreddit.
+ leave_mod_notice = True
+```
+
+You then simply run the bot:
+`python3 AlteredHeadlineBot.py`
+
+I run it in `screen` since I'm ususlly watching the ouput. It'll also write the processed events to a local database so it's not sending repeat alerts if it needs to reprocess. 
+
+**Sample Output:**
+```
+Username: XXXXX
+Stripped URL: https://www.mlive.com/public-interest/2020/04/michigan-greenhouses-garden-centers-submit-may-1-reopening-plan-to-governor.html
+Post Title: Michigan greenhouses, garden centers submit May 1 reopening plan to governor
+Actual Title: Michigan greenhouses, garden centers submit May 1 reopening plan to governor - mlive.com
+Similarity: 93
+
+Username: XXXXX
+Stripped URL: https://www.spinalcolumnonline.com/articles/oakland-county-now-offering-drive-thru-covid-19-testing/
+Post Title: Oakland country now offering drive-thru testing. Phone screening required.
+Actual Title: Oakland County now offering drive-thru COVID-19 testing | The Spinal Column
+Similarity: 74
+
+Username: XXXXX
+Stripped URL: https://www.independent.co.uk/news/world/americas/coronavirus-michigan-girl-dies-protest-lockdown-skylar-herbert-a9475216.html
+Post Title: Five-year-old daughter of Michigan emergency workers dies of coronavirus as residents continue to protest lockdown
+Actual Title: Five-year-old daughter of Michigan emergency workers dies of coronavirus as residents continue to protest lockdown | The Independent
+Similarity: 93
+
+Username: XXXXX
+Stripped URL: https://www.thedailybeast.com/devos-has-deep-ties-to-michigan-protest-group-but-is-quiet-on-tactics
+Post Title: DeVos Has Deep Ties to Michigan Protest Group, But Is Quiet On Tactics
+Actual Title: DeVos Has Deep Ties to Michigan Protest Group, But Is Quiet On Tactics
+Similarity: 100
+```
+
+**Sample Mod Notification:**
+```
+Actual Title: Finley: How to do business, and stay safe
+
+Posted Title: Autocam Medical has instituted rigorous precautions to protect worker safety
+
+Similarity: 36%
+
+Link to post for review.
+
+Please contact u/{bot_owner} if this bot is misbehaving.
+```
+
+**Sample Comment Notification:**
+```
+Hello u/{username}!
+
+The title of your post differs from the actual article title and has been flagged for review. Please review Rule #6 
+in the r/Michigan subreddit rules. If this is an actual rule violation, you can always delete the submission and 
+resubmit with the correct headline. Otherwise, this will likely be removed by the moderators. Please note that 
+some websites change their article titles and this may be a false-positive. In that case, no further action is 
+required. Further details:
+
+Posted Title: TESTING 123 TESTING 123 TESTING 123
+
+Actual Title: Lions' fourth-round pick Logan Stenberg cites 'nastiness' as one of his best traits
+
+Similarity: 24%
+
+Please contact u/{botowner} if this bot is misbehaving.
+```
